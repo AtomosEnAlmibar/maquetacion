2a32b4259105b9b65967a42ed3ba2ac64c14f769
AtomosEnAlmibar <jcmmnbvcxz@gmail.com>
2021-06-02 15:58:22 +0200

    casi done

---
+++ resources/js/desktop/front/shop.js
@@ -0,0 +1,16 @@
+//var card = document.querySelector('.card');
+var buyButtons = document.querySelectorAll('.buy-now');
+
+buyButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        console.log("polla");
+    })
+})
+/*
+card.addEventListener( 'mouseenter', function() {
+    card.classList.toggle('is-flipped');
+});
+
+card.addEventListener( 'mouseleave', function() {
+    card.classList.toggle('is-flipped');
+});*/
--- resources/views/front/shop/index.blade.php
+++ resources/views/front/shop/index.blade.php
@@ -2,13 +2,31 @@
 
 @section('content') 
     <div class="scene">
-        <div class="card">
-            <div class="front-card --front">
-                front
+        <div class="card is-flipped">
+            <div class="card-face --front">
+                <img src="https://cdn.shopify.com/s/files/1/1192/1400/products/hirudo-medicinalis_1024x1024.jpg?v=1567280772">
             </div>
 
-            <div class="back-card --back">
-                back
+            <div class="card-face --back">
+                <div class="info">
+                    <div class="title">
+                        <span>20 leech live pack</span>
+                    </div>
+                    <div class="leech-name">
+                        <span>European medicinal leech</span>
+                    </div>
+                    <div class="description">
+                        <span>Medicinal leeches have been found to secrete saliva containing about 60 different proteins. These achieve a wide variety of goals useful to the leech as it feeds, helping to keep the blood in liquid form and increasing blood flow in the affected area.</span>
+                    </div>
+                    <div class="buy">
+                        <div class="buy-item">
+                            <button class="buy-now"><span>Comprar ya</span></button>
+                        </div>
+                        <div class="price">
+                            <span>350€</span>
+                        </div>                        
+                    </div>
+                </div>
             </div>
         </div>
     </div>
