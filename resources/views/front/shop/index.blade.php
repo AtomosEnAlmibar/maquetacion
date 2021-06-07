ade16daee1674d89bfa11c71b8d7f8bd17f8f9b3
AtomosEnAlmibar <jcmmnbvcxz@gmail.com>
2021-06-07 12:14:09 +0200

    Cosillas

--- resources/views/front/shop/index.blade.php
+++ resources/views/front/shop/index.blade.php
@@ -5,26 +5,26 @@
         <div class="scene">
             <div class="card">
                 <div class="card-face --front">
-                    <img src="https://cdn.shopify.com/s/files/1/1192/1400/products/hirudo-medicinalis_1024x1024.jpg?v=1567280772">
+                    
                 </div>
     
                 <div class="card-face --back">
                     <div class="info">
                         <div class="title">
-                            <span>20 leech live pack</span>
+                            <span></span>
                         </div>
                         <div class="leech-name">
-                            <span>European medicinal leech</span>
+                            <span></span>
                         </div>
                         <div class="description">
-                            <span>Medicinal leeches have been found to secrete saliva containing about 60 different proteins. These achieve a wide variety of goals useful to the leech as it feeds, helping to keep the blood in liquid form and increasing blood flow in the affected area.</span>
+                            <span></span>
                         </div>
                         <div class="buy">
                             <div class="buy-item">
                                 <button class="buy-now"><span>Comprar ya</span></button>
                             </div>
                             <div class="price">
-                                <span>350€</span>
+                                <span></span>
                             </div>                        
                         </div>
                     </div>
